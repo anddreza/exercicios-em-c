88ecd5a3dd4a0be9d3891db6e2f44937d20e536a
guiwestrup <westrupguilherme@gmail.com>
2017-08-29 15:37:36 -0300

    Inserção exercícios 5 e 6

--- exercicio05/main.c
+++ exercicio05/main.c
@@ -1,8 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+/* 5. Construa um programa que define um tipo novo de dados, 
+para uma estrutura de um animal, deverá conter atributos como: raça, cor, peso e sexo.  
+E solicite ao usuário do programa 10 espécies e depois imprima em tela. */
+typedef char Vetor[10][40];
+typedef float Vetor2[10];
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
+typedef struct animal{
+	Vetor raca;
+	Vetor cor;
+	Vetor2 peso;
+	Vetor sexo;	
+}Animal;
+//Método para cadastrar os animais
+void cadastro(Animal *a)
+{
+	int i;
+	for (i=0;i<3;i++)
+	{
+	printf ("Digite o nome da Raça: \n");
+	scanf ("%s",a->raca[i]);
+	fflush(stdin);
+	printf ("Digite a Cor: \n");
+	scanf ("%s", a->cor[i]);
+	fflush(stdin);
+	printf ("Digite o peso: \n");
+	scanf ("%f", &a->peso[i]);
+	fflush(stdin);
+	printf ("Digite a sexo M ou F: \n");
+	scanf ("%s", a->sexo[i]);
+	fflush(stdin);
+	}
+}
+void imprime(Animal *a)
+{
+	System("cls");
+	int i;
+	for(i=0;i<3;i++) 
+	{
+		printf ("_________________________");
+		printf ("Nome da Raça: %s \n",a->raca[i]);
+		printf ("Nome da Cor: %s \n",a->cor[i]);
+		printf ("Nome da peso: %.2f \n",a->peso[i]);
+		printf ("Nome da sexo: %s \n",a->sexo[i]);
+	}
+}
+void main() {
+	
+	setlocale(LC_ALL,"");
+	Animal a;
+	cadastro(&a);
+	imprime(&a);
 }
